238c1ff681504e266e563a9d696d673784d59f8b
JeremAIh <esiebomaj@gmail.com>
2020-06-20 16:09:40 +0100

    added media to urls without if statement for debug

--- bleautech/urls.py
+++ bleautech/urls.py
@@ -28,11 +28,7 @@ urlpatterns = [
     path('', include(base_urls)),
     
 
-] 
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
